958bd6837fc21f3352c45a893a71cc5114c3f322
Maciej Krawczyk <wirus15@gmail.com>
2013-10-26 19:41:54 +0200

    wylaczone web-logi

--- app/config/common.php
+++ app/config/common.php
@@ -61,8 +61,8 @@ return array(
             'class' => 'CLogRouter',
             'routes' => array(
                 array(
-                    'class' => 'CWebLogRoute',
-                    'levels' => 'trace, error, warning',
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
                 ),
             ),
         ),
